51249b42426384d3fe6d64fe593a0c0e0d821ec2
Alonso R <luis.alonso.16@hotmail.com>
2019-08-19 16:43:39 -0600

    Update local auth

--- src/common/config/passport.config.ts
+++ src/common/config/passport.config.ts
@@ -3,25 +3,27 @@ import passportLocal from "passport-local";
 import passportFacebook from "passport-facebook";
 import { UserRepository } from '../../infrastructure/repositories/user.repository'
 import { AuthService } from "../../services/auth.service";
+import IUserRepository from "../../core/repositories/user.repository";
+import { Sequelize } from "sequelize";
 
 const LocalStrategy = passportLocal.Strategy;
 const FacebookStrategy = passportFacebook.Strategy;
-const _userRepository = new UserRepository();
+const _userRepository: IUserRepository = new UserRepository();
 
 export default () => {
     passport.serializeUser<any, any>((user, done) => {
         done(undefined, user.id);
     });
     
-    passport.deserializeUser((id: any, done) => {
+    passport.deserializeUser<any, any>((id, done) => {
         _userRepository.GetById(id).then(user => done(null, user)).catch(e => done(e, false));
     });
     
     /**
-     * Sign in using Email and Password.
+     * Sign in using username and password.
      */
     passport.use(new LocalStrategy((username, password, done) => {
-        _userRepository.FindOne({ username: username.toLowerCase() }).then(user => {
+        _userRepository.FindOne(Sequelize.or({ username: username.toLowerCase() }, { email: username.toLowerCase() })).then(user => {
             if (!user) return done(undefined, false, { message: 'User not found' })
     
             return AuthService.verifyPassword(password, user.password) ? done(undefined, user) : done(undefined, false, { message: "Invalid username or password." });
